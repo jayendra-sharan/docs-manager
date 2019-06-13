cfd00d35a71b4f6a8a42876467e86b543045a708
Jayendra <jayendra.sharan@gmail.com>
2019-06-14 00:26:10 +0530

    added file upload input

--- src/components/sidebar/sidebar-header.jsx
+++ src/components/sidebar/sidebar-header.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUpload } from '@fortawesome/free-solid-svg-icons'
 
 export default function SidebarHeader() {
+  const inpRef = useRef();
   return (
     <div className="sidebar-header">
       <div className="sidebar-header-text">Files</div>
-      <div className="upload-button">
+      <div
+        onClick={() => inpRef.current.click()}
+        className="upload-button"
+      >
         <div className="upload-btn-text">Upload</div>
         <div className="upload-icon">
           <FontAwesomeIcon icon={faUpload} />
+          <input ref={inpRef} className="hide" type="file" onChange={(e) => {
+            alert('File will be uploaded')
+          }} />
         </div>
       </div>
     </div>
