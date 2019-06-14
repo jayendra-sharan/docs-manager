05315707f92c0c3a2d4c4733f6ef21803c462cdc
Jayendra <jayendra.sharan@gmail.com>
2019-06-14 20:38:10 +0530

    added fake file uploader

--- src/app-state.js
+++ src/app-state.js
@@ -1,6 +1,20 @@
 import { Container } from 'unstated';
 import docsData from './mock/data';
 
+const getRandomDocId = () => parseInt((Math.random() + 10) * 100);
+
+const extractFileInfo = (eventObj) => {
+  const file = eventObj.target.files[0];
+  const path = URL.createObjectURL(file);;
+  const { name, type } = file;
+  return {
+    fileName: name,
+    fileType: type,
+    filePath: path,
+  };
+}
+
+
 export default class AppState extends Container {
   state = {
     docList: docsData,
@@ -12,5 +26,19 @@ export default class AppState extends Container {
     this.setState({
       selectedDoc: doc,
     });
+  }
+
+  fakeUploadDocument(eventObj) {
+    const doc = extractFileInfo(eventObj);
+    this.setState({
+      docList: [...this.state.docList, {
+        id: `doc_${getRandomDocId()}`,
+        fileName: doc.fileName,
+        filePath: doc.filePath,
+        fileType: doc.fileType.replace("application/", ""),
+        author: "me",
+        sharedWith: "Dusting",
+      }]
+    })
   }
 }
--- src/components/sidebar/sidebar-header.jsx
+++ src/components/sidebar/sidebar-header.jsx
@@ -1,24 +1,34 @@
 import React, {useRef} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUpload } from '@fortawesome/free-solid-svg-icons'
+import { Subscribe } from 'unstated';
+import AppState from '../../app-state';
 
 export default function SidebarHeader() {
   const inpRef = useRef();
   return (
-    <div className="sidebar-header">
-      <div className="sidebar-header-text">Files</div>
-      <div
-        onClick={() => inpRef.current.click()}
-        className="upload-button"
-      >
-        <div className="upload-btn-text">Upload</div>
-        <div className="upload-icon">
-          <FontAwesomeIcon icon={faUpload} />
-          <input ref={inpRef} className="hide" type="file" onChange={(e) => {
-            alert('File will be uploaded')
-          }} />
-        </div>
-      </div>
-    </div>
+    <Subscribe to={[AppState]}>
+      {
+        (appState) => (
+          <div className="sidebar-header">
+            <div className="sidebar-header-text">Files</div>
+            <div
+              onClick={() => inpRef.current.click()}
+              className="upload-button"
+            >
+              <div className="upload-btn-text">Upload</div>
+              <div className="upload-icon">
+                <FontAwesomeIcon icon={faUpload} />
+                <input
+                  ref={inpRef}
+                  className="hide"
+                  type="file"
+                  onChange={(event) => appState.fakeUploadDocument(event)} />
+              </div>
+            </div>
+          </div>
+        )
+      }
+    </Subscribe>
   )
 }
